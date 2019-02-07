72d7776d9444e6d23f45b587641e926cd62c7a45
kywang8 <35706598+kywang8@users.noreply.github.com>
2019-02-07 13:26:51 -0500

    hw 1

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # CIM542-PHYSICAL-COMPUTING
+
+## H1
+
+<a href = "/homework/spaceship_interface.ino">Link to spaceship code</a>
+
+<br>
+<a href = "https://vimeo.com/315934051">Link to spaceship video</a>
+
+<br>
+<img src = "/homework/prototype.jpg" width = "400">
+
+[link text](url)
+
+![alt tag](image_url)
--- homework/spaceship_interface.ino
+++ homework/spaceship_interface.ino
@@ -1,30 +1,32 @@
-//LEDs
+// variables for the LEDs
 const int redLed1=5;
 const int redLed2=4;
 const int greenLed=3;
 
-//switch
+//variable for the switch
 const int switchPin=2;
 
 void setup() {
-  // declare the LED pins as outputs
+  // declare LED pins as outputs
   pinMode(redLed1, OUTPUT);
   pinMode(redLed2, OUTPUT);
   pinMode(greenLed, OUTPUT);
 
-  //declare the switch pin as an input
+  //declare switch pin as input
   pinMode(switchPin, INPUT);
 }
 
 void loop() {
+  // create local variable to hold state of the switch.
   int switchState;
 
   // read the value of the switch
-  // digitalRead() checks to see if there is voltage on pin
   switchState=digitalRead(switchPin);
 
   // if the button is not pressed, blink the red LEDs
   if(switchState == LOW){
+      // digitalWrite() is the function that pulls an OUTPUT pin
+      // to 5V or 0V
 
   //turn the green LED off
   digitalWrite(greenLed, LOW);
@@ -32,6 +34,7 @@ void loop() {
   //alternately turn the red lights on/off
   digitalWrite(redLed1, HIGH);
   digitalWrite(redLed2, LOW);
+
   delay(250);
 
   digitalWrite(redLed1, LOW);
